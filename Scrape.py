652700e1c41848e44959268fba5fd56db9ab2078
Qais <shafarnaz@gmail.com>
2021-08-11 18:24:34 -0700

    Updated Scrape.py with comments and readability

--- Scrape.py
+++ Scrape.py
@@ -1,10 +1,13 @@
+# import modules
 import pprint
 import requests
 from bs4 import BeautifulSoup
 
+# determine number of pages to scrape
 pgcount = input('How many Pages? ')
 
 
+# change page to scrape the next page
 def get_page(count):
     res = requests.get(f'https://news.ycombinator.com/news?p={count}')
     web = BeautifulSoup(res.text, 'html.parser')
@@ -13,23 +16,25 @@ def get_page(count):
     return links, subtext
 
 
-def story_sort(hnlist):
-    return sorted(hnlist, key=lambda k: k['vote'], reverse=True)
-
-
-def create_custom_hn():
-    hn = []
-    for count in range(1, int(pgcount)+1):
+# use scraped data and "clean", leaving only articles within given params
+def clean_scraped_page():
+    articles = []
+    for count in range(1, int(pgcount) + 1):  # repeat for each page
         links, subtext = get_page(count)
         for idx, item in enumerate(links):
             title = item.getText()
             href = item.get('href', None)
             vote = subtext[idx].select('.score')
-            if len(vote):
+            if len(vote):  # ignore articles with no votes at all to avoid errors
                 points = int(vote[0].getText().replace(' points', ''))
-                if points > 99:
-                    hn.append({'title': title, 'link': href, 'vote': points})
-    return story_sort(hn)
+                if points > 99:  # only append articles with over 100 votes
+                    articles.append({'title': title, 'link': href, 'vote': points})
+    return story_sort(articles)
 
 
-pprint.pprint((create_custom_hn()))
+# sort scraped and "cleaned" articles for easier viewing
+def story_sort(articles):
+    return sorted(articles, key=lambda k: k['vote'], reverse=True)
+
+
+pprint.pprint((clean_scraped_page()))
